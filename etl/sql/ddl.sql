33a630b732295c1f23fb2bfa68d210ac4815be5b
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-05-15 22:18:06 -0400

    Finalizada transformação de carga no db

---
+++ etl/sql/ddl.sql
@@ -0,0 +1,25 @@
+CREATE TABLE titanic
+(
+  PassengerId integer
+, Survived integer
+, Pclass integer
+, "name" character varying(160)
+, Sex character varying(8)
+, Age float
+, SibSp integer
+, Parch integer
+, Ticket character varying(30)
+, Fare float
+, Cabin character varying(30)
+, Embarked character varying(1)
+, FamilyName character varying(30)
+, Title character varying(30)
+, FirstName character varying(100)
+, TitleClass integer
+, FamilySize integer
+, AgeClassDesc character varying(15)
+, AgeClass integer
+, SexClass integer
+, Dataset character varying(10)
+)
+;
